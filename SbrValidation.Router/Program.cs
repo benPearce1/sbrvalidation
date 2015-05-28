98d2a56551c84f6e995cec892ca0c6ee567d0d96
Ben Pearce <benpearce@yahoo.com>
2015-05-28 16:54:36 +1000

    updated gitignore for handling nuget

---
+++ SbrValidation.Router/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka;
+using Akka.Actor;
+
+namespace SbrValidation.Router
+{
+    class Program
+    {
+        public static ActorSystem RouterActorSystem;
+
+        static void Main(string[] args)
+        {
+            RouterActorSystem = ActorSystem.Create("router");
+        }
+
+    }
+}
